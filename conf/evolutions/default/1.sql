4adb11ab9d3e1aa600764dc24b114212200633bf
verteks <sendmydog@mail.ru>
2017-06-28 22:40:58 +0800

    foo fight

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -12,18 +12,18 @@ create table s3file (
   constraint pk_s3file primary key (id))
 ;
 
-create table user (
+create table XonamiUser (
   email                     varchar(255) not null,
   password_hash             varchar(255),
   salt                      varchar(255),
   easy_access_hash          varchar(255),
   easy_salt                 varchar(255),
-  constraint pk_user primary key (email))
+  constraint pk_XonamiUser primary key (email))
 ;
 
-create sequence user_seq;
+create sequence XonamiUser_seq;
 
-alter table s3file add constraint fk_s3file_user_1 foreign key (user_email) references user (email) on delete restrict on update restrict;
+alter table s3file add constraint fk_s3file_user_1 foreign key (user_email) references XonamiUser (email) on delete restrict on update restrict;
 create index ix_s3file_user_1 on s3file (user_email);
 
 
@@ -34,9 +34,9 @@ SET REFERENTIAL_INTEGRITY FALSE;
 
 drop table if exists s3file;
 
-drop table if exists user;
+drop table if exists XonamiUser;
 
 SET REFERENTIAL_INTEGRITY TRUE;
 
-drop sequence if exists user_seq;
+drop sequence if exists XonamiUser_seq;
 
